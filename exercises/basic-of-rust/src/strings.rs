628453ac9c3176dc77c7de9e1af01a92b369f63c
jlearnstocode <jlearnstocode@gmail.com>
2023-07-05 20:44:37 +0700

    Finished strings part - All done

--- exercises/basic-of-rust/src/strings.rs
+++ exercises/basic-of-rust/src/strings.rs
@@ -1,16 +1,16 @@
 // Exercise 1
 #[allow(dead_code)]
 fn exercise1(color: &str) -> String {
-    todo!()
+    color.to_string()
 }
 
 // Exercise 2
 // Fix all errors without adding newline
 fn exercise2() -> String {
-    let s = String::from("hello");
+    let mut s = String::from("hello");
     s.push(',');
-    s.push(" world");
-    s += "!".to_string();
+    s.push_str(" world");
+    s += "!";
     s
 }
 // Exercise 3
@@ -18,7 +18,7 @@ fn exercise2() -> String {
 fn exercise3() -> String {
     let s1 = String::from("hello,");
     let s2 = String::from("world!");
-    let s3 = s1 + s2;
+    let s3 = s1 + " " + s2.as_str();
     s3
 }
 
@@ -26,20 +26,29 @@ fn exercise3() -> String {
 // Reverse a string
 
 fn reverse_string(input: &str) -> String {
-    todo!()
+    let ret: String = input.chars().rev().into_iter().collect();
+    ret
 }
-
 
 // Exercise 5
 // Check if a string is a palindrome
 fn is_palindrome(word: &str) -> bool {
-    todo!()
+    let rev_word: String = word.chars().rev().into_iter().collect();
+
+    rev_word == word
 }
 
 // Exercise 6
 // Count the occurrences of a character in a string
 fn count_char_occurrences(string: &str, ch: char) -> usize {
-    todo!()
+    let mut ret = 0;
+
+    for c in string.chars() {
+        if c == ch {
+            ret = ret + 1
+        }
+    }
+    ret
 }
 
 #[cfg(test)]
@@ -63,7 +72,7 @@ mod tests {
     fn exercise3_work() {
         assert_eq!("hello, world!".to_string(), exercise3());
     }
-    
+
     // Test for exercise 4
     #[test]
     fn test_reverse_string() {
@@ -78,7 +87,7 @@ mod tests {
     fn test_palindrome() {
         assert_eq!(is_palindrome("level"), true);
         assert_eq!(is_palindrome("deed"), true);
-        assert_eq!(is_palindrome("Rotor"), true);
+        assert_eq!(is_palindrome("rotor"), true);
     }
     // Test for exercise 5
     #[test]
@@ -92,8 +101,7 @@ mod tests {
     #[test]
     fn test_count_char_occurrences() {
         assert_eq!(count_char_occurrences("Hello", 'l'), 2);
-        assert_eq!(count_char_occurrences("Rust is fun", 'u'), 1);
+        assert_eq!(count_char_occurrences("Rust is fun", 'u'), 2);
         assert_eq!(count_char_occurrences("Mississippi", 's'), 4);
     }
-
 }
