f586813b8d813536d48bd591384f89b2173fec76
jlearnstocode <jlearnstocode@gmail.com>
2023-07-11 06:03:29 +0700

    Finished structs part

--- exercises/complex-type/src/structs.rs
+++ exercises/complex-type/src/structs.rs
@@ -2,6 +2,7 @@
 // Fix the error
 // Make it compile
 // Run test
+#[derive(PartialEq, Eq, Debug)]
 struct Person {
     name: String,
     age: u8,
@@ -9,7 +10,7 @@ struct Person {
 }
 fn exercise1() -> Person {
     let age = 30;
-    // Hobby = Rust 
+    // Hobby = Rust
     let p = Person {
         name: String::from("sunface"),
         age,
@@ -39,12 +40,13 @@ impl Agent {
 
     // Get the name of the person
     fn get_name(&self) -> &str {
-        todo!()
+        &self.name
     }
 
     // Get the age of the person
     fn get_age(&self) -> u32 {
-        todo!()
+        let age = &self.age;
+        *age
     }
 }
 
@@ -61,25 +63,25 @@ impl Calculator {
         Calculator { value: 0 }
     }
 
-    fn add(&self, num: i32) {
+    fn add(&mut self, num: i32) {
         self.value += num;
     }
 
-    fn subtract(mut self, num: i32) {
+    fn subtract(&mut self, num: i32) {
         self.value -= num;
     }
-    fn clear(self) {
+
+    fn clear(&mut self) {
         self.value = 0;
     }
 
-    fn get_value(self) -> i32 {
+    fn get_value(&self) -> i32 {
         self.value
     }
 }
 
 // Exercise 4
 // Make it compile
-#[derive(Debug)]
 struct User {
     first: String,
     last: String,
@@ -93,13 +95,13 @@ fn exercise4() {
         age: 22,
     };
 
-    let u2 = User {
+    let _u2 = User {
         first: String::from("Mary"),
         ..u1
-        
+
     };
 
-    println!("user: {:#?}", u1);
+    println!("user: {:#?}", u1.first);
 
 }
 
@@ -121,11 +123,11 @@ fn exercise5() {
         int_val: 20,
     });
 
-    
-    let moved = foos[0];
 
-    
-    let moved_field = foos[0].str_val;
+    let _moved = &foos[0];
+
+
+    let _moved_field = &foos[0].str_val;
 }
 
 // Exercise 6
@@ -153,12 +155,14 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
+        self.recipient_country != self.sender_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
         // Something goes here...
+        self.weight_in_grams * cents_per_gram
     }
 }
 
@@ -174,10 +178,10 @@ mod tests {
         let p_expectation = Person {
             name: String::from("sunface"),
             age: 30,
-            hobby:String::from("Rust") 
+            hobby:String::from("Rust")
         };
         assert_eq!(p, p_expectation);
-        
+
     }
 
     // Test for exercise 2
